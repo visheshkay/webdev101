cd2e1e5809e784d29240eff8d65269032e7de37b
Vishesh Kondaveeti <68546456+visheshkay@users.noreply.github.com>
2025-02-23 11:28:53 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,13 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDHkMPcbmLrLkDGcGfsKm4Zz4Pp8g8DO0k",
-  authDomain: "eventplanner-2a17a.firebaseapp.com",
-  projectId: "eventplanner-2a17a",
-  storageBucket: "eventplanner-2a17a.firebasestorage.app",
-  messagingSenderId: "240614341561",
-  appId: "1:240614341561:web:0c21b2d8ff588a161b1468",
-  measurementId: "G-TCLR65VPZC"
+ //your details from web
 };
 
 
